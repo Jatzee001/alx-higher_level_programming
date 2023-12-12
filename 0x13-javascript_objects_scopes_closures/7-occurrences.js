3d5ac4301e8fc9084110f7179f5e82b1ce8fe1f7
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-12-12 08:25:50 +0000

    7. Occurrences

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+
+exports.nbOccurences = function (list, searchElement) {
+    // Count the number of occurrences of searchElement in the list
+    return list.reduce((count, element) => (element === searchElement ? count + 1 : count), 0);
+  };  
