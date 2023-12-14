c0c594bcc0e1646919b9b3dc17fcfcad6515f416
Jatzee001 <133218924+Jatzee001@users.noreply.github.com>
2023-12-14 07:49:56 +0000

    12. Update a state

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""Changes the name of a State object from the database hbtn_0e_6_usa"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    # Check if the correct number of arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Extract command line arguments
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    # Connect to MySQL server
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(username, password, database), pool_pre_ping=True)
+
+    # Create a session to interact with the database
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    # Query the State object with id=2 and change its name to "New Mexico"
+    state_to_update = session.query(State).filter_by(id=2).first()
+    if state_to_update:
+        state_to_update.name = "New Mexico"
+        session.commit()
+
+    # Close the session
+    session.close()
